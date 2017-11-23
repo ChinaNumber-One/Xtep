f94e6ee88f30849bf7eec00e87ed421cfba0a636
Yujiaming <1026993235@qq.com>
2017-11-23 09:34:41 +0800

    ok

--- js/shopcar.js
+++ js/shopcar.js
@@ -10,9 +10,11 @@ window.onload=function(){
 			"background":"url(img/common-head.png) no-repeat center center",
 			"backgroundSize":"32px 32px"
 			})
+	}else{
+		location.href="login.html"
 	}
 	if(getCookie("shopcar")){
-		var shopcar=JSON.parse(getCookie("shopcar"))
+		shopcar=JSON.parse(getCookie("shopcar"))
 		for(var i=0;i<shopcar.length;i++){
 			$(`<tr>
 					<td><input type="checkbox" class="check1" style="margin-left: 60px;"/></td>
@@ -23,15 +25,37 @@ window.onload=function(){
 						<p>颜色<span class="sccolor">${shopcar[i].color}</span>尺寸<span class="sccolor">${shopcar[i].size}</span></p>
 						</div>
 					</td>
-					<td>￥${shopcar[i].price}</td>
+					<td>${shopcar[i].price}</td>
 					<td style="text-align: center;">
 						<div class="num">
-							<b>-</b><input type="text" value="${shopcar[i].count}"/><b>+</b>
+							<b>-</b><input type="text" class="shopnum" value="${shopcar[i].count}"/><b>+</b>
 						</div>
 					</td>
-					<td>￥${shopcar[i].price*shopcar[i].count}</td>
-					<td><a href="javascript:;">加入收藏</a><a href="javascript:;">删除</a></td>
+					<td class="sum">${shopcar[i].price*shopcar[i].count}</td>
+					<td><a href="javascript:;">加入收藏</a><a href="javascript:;" class="delcar">删除</a></td>
 				</tr>`).appendTo(".shopcar table tbody");
 		}
 	}
 }
+$(".shopcar table tbody").on("click",".delcar",function(){
+	var delindex=$(this).parent().parent().index()
+	console.log(delindex)
+	$(this).parent().parent().remove();
+	shopcar.splice(delindex-1,1)
+	if(shopcar.length!=0){
+		setCookie("shopcar",JSON.stringify(shopcar))
+	}else{
+		setCookie("shopcar","",-1)
+	}
+})
+var num=0;
+var sump=0
+$(".shopcar table tbody").on("click",".check1",function(){
+	num+=Number($(this).parent().parent().find(".shopnum").val());
+	sump+=Number($(this).parent().parent().find(".sum").html())
+	$(".payfor i").html(num)
+	$(".payfor em").html(sump+"元")
+	console.log($("table tbody tr").size())
+	
+})
+
